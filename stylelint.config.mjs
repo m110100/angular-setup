ecb8fe917fc3cecead9d6fc5b3ea9f2a1a9fde89
m110100 <shindayoni@gmail.com>
2025-02-15 20:13:49 +0300

    chore: config done

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -8,13 +8,23 @@ export default {
 		'stylelint-config-standard',
 		'stylelint-config-rational-order',
 		'stylelint-prettier/recommended',
+		'stylelint-config-standard-scss',
+		'stylelint-config-prettier-scss',
 	],
 	files: ['**/*.css', '**/*.scss', '**/*.less'],
 	rules: {
 		'at-rule-no-unknown': null,
-		'at-rule-empty-line-before': null,
-		'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
 		'scss/at-rule-no-unknown': true,
+		'selector-class-pattern':
+			'^(([a-z][a-z0-9]*)(-[a-z0-9]+)*)|(([A-Z][a-z]+)+(_([A-Z][a-z]+)+)*((_[a-z]*))*)$',
+		'scss/percent-placeholder-pattern': '^(v-(.*))$',
+		'scss/at-extend-no-missing-placeholder': null,
+		'color-function-notation': ['modern', { ignore: ['with-var-inside'] }],
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{ ignorePseudoClasses: ['global'] },
+		],
+		'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
 		'scale-unlimited/declaration-strict-value': [
 			[
 				'color',
@@ -31,8 +41,10 @@ export default {
 					'inherit',
 					'initial',
 					'transparent',
+					'rem',
 				],
 			},
 		],
+		'declaration-property-value-no-unknown': null,
 	},
 };
